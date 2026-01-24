930071af01ebb93a37bb8964334cd129a405c507
Prem1275 <prembhalerao1275@gmail.com>
2026-01-25 00:14:22 +0530

    Added Prem changes

---
+++ script.js
@@ -0,0 +1,12 @@
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 4000); // change slide every 4 seconds
